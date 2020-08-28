e178c4f53d8e73d0bd6c958bb2da83f0f650cb21
Hakan Acundaş <50059772+HakanAcundas@users.noreply.github.com>
2020-08-28 21:39:10 +0300

    Add files via upload

---
+++ ShortestPath(Genetic Algorithm)/Java/Main.java
@@ -0,0 +1,37 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Evolution e = new Evolution();
+		e.evolve(1);
+		e.evolve(1);
+		e.evolve(1);
+		e.evolve(1);
+		
+		int [] a = {1,2,3};
+		String[] b = {"3","4","5"};
+	
+		//Main ab = new Main();
+		/*ab.s(3, 4);
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i]);
+		}*/
+	}
+	
+	
+	/*public int[] add_new_ele(int[] old_list, int x) {
+		int [] new_list = new int[old_list.length + 1];
+		for (int i = 0; i<old_list.length; i++) {
+			new_list[i] = old_list[i];
+		}
+		new_list[old_list.length] = x;
+		old_list = new_list;
+		return old_list;
+	}
+	
+	public int s(int a, int t) {
+		a = t;
+		return a;
+	}*/
+}
